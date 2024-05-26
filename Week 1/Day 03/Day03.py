b3fff5cad71f853df2c692aa37dab134d02c66c9
Krushnal121 <krushnalpatil1111@gmail.com>
2024-05-26 18:41:34 +0530

    Commit

---
+++ Week 1/Day 03/Day03.py
@@ -0,0 +1,19 @@
+# Creating and using sets
+unique_numbers = {1, 2, 3, 2, 1}
+print(unique_numbers)  # Output: {1, 2, 3}
+
+# String manipulation
+message = "Hello, Python!"
+print(message.lower())  # Output: hello, python!
+print(message.upper())  # Output: HELLO, PYTHON!
+print(message.replace("Python", "World"))  # Output: Hello, World!
+
+# Using if, elif, and else
+age = 18
+if age < 18:
+    print("You are a minor.")
+elif age == 18:
+    print("You just became an adult.")
+else:
+    print("You are an adult.")
+
