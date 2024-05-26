62a75be2f2d3f3fc40a725ba75b98774e7b256fd
Krushnal121 <krushnalpatil1111@gmail.com>
2024-05-26 18:39:32 +0530

    Commit

---
+++ Week 1/Day 01/Day01.py
@@ -0,0 +1,12 @@
+# Example of a simple print statement after installing Python
+print("Hello, Python!")
+
+# Variables and basic arithmetic
+a = 5
+b = 10
+sum = a + b
+print("Sum:", sum)
+
+# Basic input and output
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
