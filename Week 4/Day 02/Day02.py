6eb4fbc4a37166cad2beeb833d7f5c6440ef7706
Krushnal121 <krushnalpatil1111@gmail.com>
2024-06-11 19:14:57 +0530

    Commit

---
+++ Week 4/Day 02/Day02.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Creating a matrix
+matrix = np.array([[1, 2], [3, 4]])
+print("Matrix:\n", matrix)
+
+# Creating a vector
+vector = np.array([1, 2, 3])
+print("Vector:", vector)
+
+# Matrix multiplication
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+product = np.dot(matrix1, matrix2)
+print("Matrix Product:\n", product)
+
